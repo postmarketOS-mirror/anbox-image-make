postmarketOS-mirror/anbox-image-make

commit cba3a6660afc86aa507c0ab7655edffc8ebcb88b

Add $CARCH support (MR 1)
fixes #1.

Makefile
@@ -7,12 +7,24 @@ CHROOT_PATH ?= /var/chroot/$(RELEASE)/
 ANBOX_SOURCE ?= ./source
 ANBOX_OUT ?= ./out
 ANBOX_IMAGE_OUT ?= .
+CARCH ?= x86_64
 # end of configuration options
 
 
 RELEASE := xenial
 MIRROR := http://archive.ubuntu.com/ubuntu/
 
+# yay conventions
+arch_aarch64 := arm64
+arch_armv7 := armv7a_neon
+arch_x86_64 := x86_64
+ARCH := $(arch_${CARCH})
+
+# yay not even following your convention
+arch_dir_aarch64 := arm64
+arch_dir_armv7 := armv7-a-neon
+arch_dir_x86_64 := x86_64
+ARCH_DIR := $(arch_dir_${CARCH})
 
 .PHONY: remove-chroot
 remove-chroot:
@@ -73,7 +85,8 @@ chroot: $(CHROOT_PATH)
 .PHONY: build
 .ONESHELL: build
 build: $(CHROOT_PATH)
-	export CHROOT_PATH=$(CHROOT_PATH) ANBOX_SOURCE=$(ANBOX_SOURCE) ANBOX_OUT=$(ANBOX_OUT)
+	export CHROOT_PATH=$(CHROOT_PATH) ANBOX_SOURCE=$(ANBOX_SOURCE) \
+		ANBOX_OUT=$(ANBOX_OUT) ARCH=$(ARCH)
 	mountpoint -q $$CHROOT_PATH/home/build/source || \
 		sudo mount $$ANBOX_SOURCE --bind -o ro \
 			$$CHROOT_PATH/home/build/source
@@ -81,13 +94,15 @@ build: $(CHROOT_PATH)
 		sudo mount $$ANBOX_OUT --bind -o rw || \
 			$$CHROOT_PATH/home/build/source/out \
 			# building out of tree is broken
+	mountpoint -q $$CHROOT_PATH/home/build/source || sudo mount $$ANBOX_SOURCE --bind -o ro $$CHROOT_PATH/home/build/source
+	mountpoint -q $$CHROOT_PATH/home/build/source/out || sudo mount $$ANBOX_OUT --bind -o rw $$CHROOT_PATH/home/build/source/out # building out of tree is broken
 	sudo arch-chroot $$CHROOT_PATH <<EOT2
 	sudo -u build -i <<EOT
 	export LC_ALL=C.UTF-8
 	export _JAVA_OPTIONS="-Xmx3072m" # increase heap size
 	cd /home/build/source
 	. build/envsetup.sh
-	lunch anbox_x86_64-userdebug
+	lunch anbox_$$ARCH-userdebug
 	make -j8
 	EOT
 	EOT2
@@ -102,6 +117,6 @@ image: build
 			/home/build/source/out/target/product/x86_64/system.img \
 			/home/build/source/out/android.img"
 	mv $(CHROOT_PATH)/home/build/source/out/android.img \
-		$(ANBOX_IMAGE_OUT)/android.img
+		$(ANBOX_IMAGE_OUT)/android-$(CARCH).img
 	@printf "Written $%s. Have a good day!" \
-		"$(ANBOX_IMAGE_OUT)/android.img"
+		"$(ANBOX_IMAGE_OUT)/android-$(CARCH).img"

commit 727ef89237d3ed7348802aa77802846695f04318

Fix first run of 'make build' (MR 3)
This error in the code comes from a rebase where some piece of code that
was moved around got duplicated. The first instance is broken, but
I didn't notice at first because the second copy worked fine.

Fixes: cba3a666 ("Add $CARCH support (MR 1)")

Makefile
@@ -154,11 +154,9 @@ build: source/Makefile $(CHROOT_PATH)
 		sudo mount $$ANBOX_SOURCE --bind -o ro \
 			$$CHROOT_PATH/home/build/source
 	mountpoint -q $$CHROOT_PATH/home/build/source/out || \
-		sudo mount $$ANBOX_OUT --bind -o rw || \
+		sudo mount $$ANBOX_OUT --bind -o rw \
 			$$CHROOT_PATH/home/build/source/out \
 			# building out of tree is broken
-	mountpoint -q $$CHROOT_PATH/home/build/source || sudo mount $$ANBOX_SOURCE --bind -o ro $$CHROOT_PATH/home/build/source
-	mountpoint -q $$CHROOT_PATH/home/build/source/out || sudo mount $$ANBOX_OUT --bind -o rw $$CHROOT_PATH/home/build/source/out # building out of tree is broken
 	sudo arch-chroot $$CHROOT_PATH <<EOT2
 	sudo -u build -i <<EOT
 	export LC_ALL=C.UTF-8